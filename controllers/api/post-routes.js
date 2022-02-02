3e2744343240243450085cfe9402c70cd0d28c2a
Andrew Febre <afebre1027@gmail.com>
2022-02-01 22:35:00 -0500

    14.5

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -85,12 +85,12 @@ router.get("/:id", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
+router.post("/", withAuth, (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
     post_url: req.body.post_url,
-    user_id: req.session.user_id,
+    user_id: req.session.user_id
   })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
@@ -99,7 +99,7 @@ router.post("/", (req, res) => {
     });
 });
 
-router.put("/upvote", (req, res) => {
+router.put("/upvote", withAuth, (req, res) => {
   // make sure the session exists first
   if (req.session) {
     // pass session id along with all destructured properties on req.body
@@ -115,7 +115,7 @@ router.put("/upvote", (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", withAuth, (req, res) => {
   Post.update(
     {
       title: req.body.title,
@@ -139,7 +139,7 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", withAuth, (req, res) => {
   console.log("id", req.params.id);
   Post.destroy({
     where: {
